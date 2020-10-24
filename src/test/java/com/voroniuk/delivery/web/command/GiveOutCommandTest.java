1536afca2a0bd016b83213946c662b7f3c43a6d6
KpKKdMtn <leftn.my@gmail.com>
2020-10-24 19:29:07 +0300

    GiveOutCommandTest added

---
+++ src/test/java/com/voroniuk/delivery/web/command/GiveOutCommandTest.java
@@ -0,0 +1,66 @@
+package com.voroniuk.delivery.web.command;
+
+import com.voroniuk.delivery.db.dao.CityDAO;
+import com.voroniuk.delivery.db.dao.OrderDAO;
+import com.voroniuk.delivery.db.dao.UserDAO;
+import com.voroniuk.delivery.db.entity.*;
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GiveOutCommandTest {
+
+
+    private OrderDAO orderDAO = new OrderDAO();
+    private CityDAO cityDAO = new CityDAO();
+    private UserDAO userDAO = new UserDAO();
+
+    private GiveOutCommand giveOutCommand = new GiveOutCommand();
+    private HttpServletRequest request = mock(HttpServletRequest.class);
+    private HttpServletResponse response = mock(HttpServletResponse.class);
+
+    @Test
+    public void shouldChangeStatusToArrived() throws IOException, ServletException {
+
+        City origin = cityDAO.findCityByName("Bar");
+        City destination = cityDAO.findCityByName("Nemyriv");
+        User user = userDAO.findUserById(2);
+        String address = "address";
+        int originId = origin.getId();
+        int destinationId = destination.getId();
+
+        Delivery delivery = new Delivery();
+        delivery.setUser(user);
+        delivery.setOrigin(origin);
+        delivery.setDestination(destination);
+        delivery.setAddress(address);
+        delivery.setType(CargoType.PARCEL);
+        delivery.setWeight(77);
+        delivery.setVolume(77);
+        delivery.setCost(77);
+        delivery.addStatus(DeliveryStatus.NEW, new Date());
+        orderDAO.saveDelivery(delivery);
+
+
+
+        when(request.getParameter("delivery_id")).thenReturn(String.valueOf(delivery.getId()));
+
+        giveOutCommand.execute(request, response);
+
+        Delivery founded = orderDAO.findDeliveryById(delivery.getId());
+
+        assertEquals(DeliveryStatus.RECIEVED, founded.getLastStatus());
+
+        orderDAO.deleteDeliveryById(delivery.getId());
+
+    }
+
+}
